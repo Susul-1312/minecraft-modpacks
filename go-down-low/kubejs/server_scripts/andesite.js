7ea567ec762abdad40af3b3e4700b5e7792a8f48
Blue <susl@rickroll.link>
2023-05-14 23:19:05 +0200

    im dumb and i hate how i cant javascript

--- go-down-low/kubejs/server_scripts/andesite.js
+++ go-down-low/kubejs/server_scripts/andesite.js
@@ -1,5 +1,10 @@
 console.log("Adjusting andesite alloys")
 
+onEvent('item.tags', event => {
+    event.add("custom:andesite", "minecraft:andesite");
+    event.add("custom:andesite", "consistency_plus:cobbled_andesite");
+})
+
 onEvent('recipes', event => {
     event.replaceInput({output: 'create:andesite_alloy'}, 'minecraft:andesite', '#forge:material/andesite');
 });
